07dfc858f874ebf074d9ae1cdbfc54db15e7443d
nurekeeenti <nurekeeeeenti@gmail.com>
2024-11-30 01:18:04 +0500

    commiti

--- aldoik.go
+++ aldoik.go
@@ -3,28 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Sam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-	fmt.Printf("Sam takoSam takoSam takoSam takoSam takoSam takoSam takoSam takoi")
-
+	fmt.Printf("Super kolbasa")
 }
 
